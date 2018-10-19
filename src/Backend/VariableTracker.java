88f2dc3aaaf3c30f99fd369ce9797112cc3c7eca
mmb70 <mmb70@duke.edu>
2018-10-19 16:56:30 -0400

    Added basic VariableTracker implementation w/ comments

--- src/Backend/VariableTracker.java
+++ src/Backend/VariableTracker.java
@@ -1,12 +1,72 @@
+/**
+ * @author Max Bartlett
+ */
+
 package Backend;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class VariableTracker {
-    public Turtle getTurtle() {
-        return myTurtle;
+    private Turtle turtle;
+    private Map<String, Object> varMap;
+
+    /**
+     * Default constructor initializes Turtle() and HashMap()
+     */
+    public VariableTracker(){
+        turtle = new Turtle();
+        varMap = new HashMap<>();
     }
 
-    private Turtle myTurtle;
-    public VariableTracker(){
-        myTurtle = new Turtle();
+    /**
+     * @return turtle
+     */
+    public Turtle getTurtle() {
+        return turtle;
     }
+
+    /**
+     * puts <Key, Value> in varMap
+     * @param key variable name
+     * @param value variable value
+     * @return value if key already exists in varMap, otherwise null
+     */
+    public Object put(String key, Object value) {
+        return varMap.put(key, value);
+    }
+
+    /**
+     * gets value associated with key from varMap
+     * @param key key to be accessed
+     * @return value if key exists in varMap, otherwise null
+     */
+    public Object get(String key) {
+        return varMap.get(key);
+    }
+
+    /**
+     * removes key from varMap
+     * @param key key to be removed
+     * @return previous value if key exists in varMap, otherwise null
+     */
+    public Object remove(String key) {
+        return varMap.remove(key);
+    }
+
+    /**
+     * @return set containing all var names
+     */
+    public Set<String> keySet() {
+        return varMap.keySet();
+    }
+
+    /**
+     * clears varMap
+     */
+    public void clear() {
+        varMap.clear();
+    }
+
 }
