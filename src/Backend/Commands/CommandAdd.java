639214196f1f6be2d1afe05907d66b7a5101a4c8
Michael Glushakov <mg367@duke.edu>
2018-10-20 20:33:07 -0400

    bug fix

--- src/Backend/Commands/CommandAdd.java
+++ src/Backend/Commands/CommandAdd.java
@@ -9,7 +9,7 @@ import java.util.List;
  * @author Michael Glushakov (mg367)
  */
 public class CommandAdd extends MathCommand{
-    private double d1,d2;
+    private Double d1,d2;
     public CommandAdd(VariableTracker tracker) {
         super(tracker);
     }
@@ -29,9 +29,10 @@ public class CommandAdd extends MathCommand{
         double temp=d1;
         System.out.println("d1: "+d1+" d2: "+d2);
         List params2=new ArrayList(params);
-        d2=parseParameters(params2);
-        params=params2;
+        d2=parseParameters(params);
+//        params=params2;
         System.out.println("d1: "+d1+" d2: "+d2+"temp: "+temp);
+        d1=temp;
         return ""+(d1+d2);
     }
 }
--- src/Backend/Tester.java
+++ src/Backend/Tester.java
@@ -6,7 +6,7 @@ import Backend.Commands.CommandManager;
 public class Tester {//Make into Junits
     public static void main (String[]args){
         CommandManager manager = new CommandManager("languages.English");
-        String testInput="SUM 12 SUM 7 6";
+        String testInput="SUM 12 SUM 7 Sum 1 2";
         String out=manager.execute(testInput);
         System.out.println("OUT:"+out);
     }
