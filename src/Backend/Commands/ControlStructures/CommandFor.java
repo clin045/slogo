818347cbfa6959586f4e40783faed9dace7afa22
Michael Glushakov <mg367@duke.edu>
2018-10-21 18:00:19 -0400

    For done

---
+++ src/Backend/Commands/ControlStructures/CommandFor.java
@@ -0,0 +1,42 @@
+package Backend.Commands.ControlStructures;
+
+import Backend.Command;
+import Backend.CommandManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommandFor extends Command {
+    private String varName;
+    @Override
+    public String getDescription() {
+        return "runs command(s) for each value specified in the range, i.e., from (start - end), going by increment";
+    }
+
+    @Override
+    public String execute(List<String> params) {
+        List<String>tempList;
+        double out=0;
+        if(!params.remove(0).equals("[")){throw new IllegalArgumentException("Limit must be enclosed by brackets []");}//[ v s e i ]
+        if(!params.remove(4).equals("]")){throw new IllegalArgumentException("FOR only takes 3 limit argument");}//0 1 2 3 4 5
+        varName=params.remove(0);
+        int start=(int)parseParameters(params);
+        int end=(int)parseParameters(params);
+        int increment=(int)parseParameters(params);
+        for(int i=start;i<=end;i+=increment){
+            CommandManager.myTracker.put(varName,(double)i);
+            tempList=new ArrayList<>(params);
+            if(!tempList.get(0).equals("[")){throw new IllegalArgumentException("FOR commands must be surrounded by square brackets");}
+            tempList.remove("[");
+            while(tempList.size()>0){
+                if(tempList.get(0).equals("]")){break;}
+                if(tempList.get(0).equals("[")){break;}
+
+                Command loopCmd=Command.getCommand(tempList.get(0));
+                tempList.remove(0);
+                out=Double.parseDouble(loopCmd.execute(tempList));
+            }
+        }
+        return ""+out;
+    }
+}
