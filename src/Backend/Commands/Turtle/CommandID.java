9abc1ac8fe01a8aebc81424cb3d811b25000cb4e
Christopher Lin <cl349@duke.edu>
2018-10-26 11:51:26 -0400

    added get id

---
+++ src/Backend/Commands/Turtle/CommandID.java
@@ -0,0 +1,21 @@
+package Backend.Commands.Turtle;
+
+import Backend.Command;
+import Backend.VariableTracker;
+
+import java.util.List;
+
+public class CommandID extends Command {
+    CommandID(VariableTracker tracker){
+        super(tracker);
+    }
+    @Override
+    public String getDescription() {
+        return "returns current active turtle's ID number";
+    }
+
+    @Override
+    public String execute(List<String> params) {
+        return Integer.toString(myTracker.getActiveTurtle().getID());
+    }
+}
