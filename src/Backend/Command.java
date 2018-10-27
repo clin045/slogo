b3c94f0fc0855dfd09a22ff20c2aee028a818b95
Christopher Lin <cl349@duke.edu>
2018-10-27 14:21:32 -0400

    refactored to extract evaluateBrackets

--- src/Backend/Command.java
+++ src/Backend/Command.java
@@ -3,6 +3,7 @@ package Backend;
 import Backend.Exceptions.InvalidInputException;
 import Backend.Exceptions.ParameterAmountException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Command {
@@ -62,6 +63,37 @@ public abstract class Command {
             params.remove(0);
         }
         return param;
+    }
+
+    protected ArrayList<Double> evaluateBrackets(List<String> exp) {
+        exp.remove("[");
+        exp.remove("]");
+        //check if each param is a command, if so, evaluate it
+        var retList = new ArrayList<Double>();
+        while(exp.size() > 0){
+            if(CommandManager.isCommand(exp.get(0))){
+                var cmd = CommandManager.getCommand(exp.get(0), myTracker);
+                exp.remove(0);
+                try{
+                    var doubVal = Double.parseDouble(cmd.execute(exp.subList(0, exp.size())));
+                    retList.add(doubVal);
+
+                }
+                catch(Exception e){
+                    throw new IllegalArgumentException("Invalid params for Tell");
+                }
+            }
+            else{
+                try{
+                    retList.add(Double.parseDouble(exp.get(0)));
+                    exp.remove(0);
+                }
+                catch(Exception e){
+                    throw new IllegalArgumentException("Invalid params for Tell");
+                }
+            }
+        }
+        return retList;
     }
     public abstract String execute(List <String>params);
 
