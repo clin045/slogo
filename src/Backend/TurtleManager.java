ea41d999d01d60e2661d6837812bba0a3cb4c9d9
Christopher Lin <cl349@duke.edu>
2018-11-02 13:29:56 -0400

    document and clean up TurtleManager

--- src/Backend/TurtleManager.java
+++ src/Backend/TurtleManager.java
@@ -7,6 +7,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Manages active turtles and executes methods on all active turtles
+ * Assumptions: Turtle implements all methods called by TurtleManager
+ * Dependencies: Turtle
+ *
+ *
+ * @author Christopher Lin
+ */
+
 public class TurtleManager {
 
     private Map<Integer, Turtle> turtleMap;
@@ -21,9 +30,18 @@ public class TurtleManager {
         turtleMap.put(1, turtle1);
     }
 
+    /**
+     *
+     * @return the total number of turtles in the turtleMap
+     */
     public int getNumTurtles() {
         return turtleMap.size();
     }
+
+    /**
+     *
+     * @return all turtles in the turtleMap
+     */
 
     public List<Turtle> getAllTurtles() {
         var allTurtles = new ArrayList<Turtle>();
@@ -33,19 +51,37 @@ public class TurtleManager {
         return allTurtles;
     }
 
+    /**
+     *
+     * @return a list of all active turtles
+     */
     public List<Turtle> getActiveTurtles() {
         return activeTurtles;
     }
 
+    /**
+     * Directly sets the active turtles to a specified list
+     * @param turtles A list of turtles
+     */
     public void setActiveTurtles(List<Turtle> turtles) {
         activeTurtles.clear();
         activeTurtles.addAll(turtles);
     }
 
+    /**
+     *
+     * @param id    Id of turtle
+     * @return  Turtle with given id
+     */
+
     public Turtle getTurtleByID(int id) {
         return turtleMap.get(id);
     }
 
+    /**
+     *
+     * @return ID of last active turtle
+     */
     public int getID() {
         return (activeTurtles.get(activeTurtles.size() - 1).getID());
     }
@@ -65,6 +101,11 @@ public class TurtleManager {
     public boolean getPenDown() {
         return (activeTurtles.get(activeTurtles.size() - 1).getPenDown());
     }
+
+    /**
+     * Sets the active turtle to a list of IDs
+     * @param turtlesIDs    Integer list of turtle IDs
+     */
 
     public void setActiveTurtlesByID(List<Integer> turtlesIDs) {
         activeTurtles.clear();
@@ -234,14 +275,11 @@ public class TurtleManager {
         return index;
     }
 
-//    public double getPenColor(){
-//        return activeTurtles.get(activeTurtles.size()-1);
-//    }
-
-    public void createTurtle(int id) {
-        Turtle newTurtle = new Turtle(id);
-        turtleMap.put(id, newTurtle);
-    }
+    /**
+     * Creates a new turtle with given id and controller
+     * @param id    id of turtle
+     * @param controller    FrontEnd controller to bind to turtle
+     */
 
     public void createTurtle(int id, Controller controller) {
         Turtle newTurtle = new Turtle(id);
